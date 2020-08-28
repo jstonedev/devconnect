4d7c1b263e60b8ca86c64476d53980e17384fc5d
jstonedev <jstonedev521@gmail.com>
2020-08-28 02:00:10 -0400

    modify: register a user with request & body validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,72 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
 
-// @route   GET api/users
-// @desc    Test route
+const User = require("../../models/User");
+
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
-router.get("/", (req, res) => res.send("User route"));
+router.post(
+	"/",
+	[
+		check("name", "Name is required").not().isEmpty(),
+		check("email", "Please include a valid email").isEmail(),
+		check(
+			"password",
+			"Please enter a password with 6 or more characters"
+		).isLength({ min: 6 }),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { name, email, password } = req.body;
+
+		try {
+			// See if user exists
+			let user = await User.findOne({ email });
+
+			if (user) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: "User already exists" }] });
+			}
+
+			// Get users gravatar
+			const avatar = gravatar.url(email, {
+				s: "200",
+				r: "pg",
+				d: "mm",
+				protocol: "https",
+			});
+
+			// Create new user instance
+			user = new User({
+				name,
+				email,
+				avatar,
+				password,
+			});
+
+			// Encrypt password
+			user.password = await bcrypt.hash(password, 10);
+
+			// Save user to DB
+			await user.save();
+
+			// Return jsonwebtoken
+
+			res.send("User registered");
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).send("Server error");
+		}
+	}
+);
 
 module.exports = router;
