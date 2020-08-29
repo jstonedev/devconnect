468c438e848489eee6bd871f60a694490761438c
jstonedev <jstonedev521@gmail.com>
2020-08-28 21:04:44 -0400

    modify: setup user authentication for login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
 
@@ -16,5 +20,76 @@ router.get("/", auth, async (req, res) => {
 		res.status(500).send("Server Error");
 	}
 });
+
+// @route   POST api/users
+// @desc    Authenticate existing user & get token
+// @access  Public
+router.post(
+	"/",
+	[
+		check("email", "Please include a valid email").isEmail(),
+		check("password", "Password is required").exists(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+
+		try {
+			// See if user exists
+			let user = await User.findOne({ email });
+
+			if (!user) {
+				return res
+					.status(400)
+					.json({
+						errors: [
+							{
+								msg:
+									"Invalid Credentials, please try again or register new account",
+							},
+						],
+					});
+			}
+
+			// Compare user password
+			const isMatch = await bcrypt.compare(password, user.password);
+			if (!isMatch) {
+				return res.status(400).json({
+					errors: [
+						{
+							msg:
+								"Invalid Credentials, please try again or register new account",
+						},
+					],
+				});
+			}
+
+			// Create payload for jsonwebtoken
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			// Sign token
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"), // secret
+				{ expiresIn: 360000 },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).send("Server error");
+		}
+	}
+);
 
 module.exports = router;
