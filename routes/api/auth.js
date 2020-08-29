7a1ca17a5350d1b69631beac4924b05bee87995b
jstonedev <jstonedev521@gmail.com>
2020-08-28 20:31:59 -0400

    modify: change route desc. New user authentication done.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,7 +5,7 @@ const auth = require("../../middleware/auth");
 const User = require("../../models/User");
 
 // @route   GET api/auth
-// @desc    Test route
+// @desc    Authenticate new users
 // @access  Public
 router.get("/", auth, async (req, res) => {
 	try {
