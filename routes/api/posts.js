a6624c89c57408a1adda65b45492035bb0ec9a2b
jstonedev <jstonedev521@gmail.com>
2020-09-10 01:10:35 -0400

    modify: add get & delete post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,7 +7,7 @@ const Post = require("../../models/Post");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 
-// @route   GET api/posts
+// @route   POST api/posts
 // @desc    Create a post
 // @access  Private
 router.post(
@@ -42,5 +42,73 @@ router.post(
 		}
 	}
 );
+
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+router.get("/", auth, async (req, res) => {
+	try {
+		const posts = await Post.find().sort({ date: -1 });
+		res.json(posts);
+	} catch (error) {
+		console.error(error);
+		res.status(500).send("Server Error");
+	}
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Private
+router.get("/:id", auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		if (!post) {
+			return res.status(404).json({ msg: "Post not found" });
+		}
+
+		res.json(post);
+	} catch (error) {
+		console.error(error);
+
+		if (error.kind === "ObjectId") {
+			return res.status(404).json({ msg: "Post not found" });
+		}
+
+		res.status(500).send("Server Error");
+	}
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+router.delete("/:id", auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// Check for post
+		if (!post) {
+			return res.status(404).json({ msg: "Post not found" });
+		}
+
+		// Check user
+		if (post.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: "User not authorized" });
+		}
+
+		// Remove post from db
+		await post.remove();
+
+		res.json({ msg: "Post successfully removed" });
+	} catch (error) {
+		console.error(error);
+
+		if (error.kind === "ObjectId") {
+			return res.status(404).json({ msg: "Post not found" });
+		}
+
+		res.status(500).send("Server Error");
+	}
+});
 
 module.exports = router;
