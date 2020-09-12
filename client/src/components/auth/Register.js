67056d05c6267d3a9aed2ea6919d1c8170878810
jstonedev <jstonedev521@gmail.com>
2020-09-11 20:39:18 -0400

    modify: make onSubmit an async function & log success message

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -14,14 +14,13 @@ export const Register = () => {
 	const onChange = (e) =>
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 
-	const onSubmit = (e) => {
+	const onSubmit = async (e) => {
 		e.preventDefault();
 
-		// check matching passwords
 		if (password !== password2) {
 			console.log("Passwords do not match");
 		} else {
-			console.log(formData);
+			console.log("SUCCESS!");
 		}
 	};
 	return (
