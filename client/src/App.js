902bc62aa4e2e3adc2407a52e9f3a7827aefb4ba
jstonedev <jstonedev521@gmail.com>
2020-09-24 11:47:27 -0400

    modify: add alert component and action call

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import { Navbar } from "./components/layout/Navbar";
 import { Landing } from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Alert from "./components/layout/Alert";
 
 const App = () => (
 	<Provider store={store}>
@@ -16,6 +17,7 @@ const App = () => (
 				<Navbar />
 				<Route exact path="/" component={Landing} />
 				<section className="container">
+					<Alert />
 					<Switch>
 						<Route path="/register" component={Register} />
 						<Route path="/login" component={Login} />
