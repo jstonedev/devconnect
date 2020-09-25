57fed12176d2d3950713674747fcbd6efe83058c
jstonedev <jstonedev521@gmail.com>
2020-09-24 20:52:26 -0400

    modify: load user & set auth token

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import setAuthToken from "./utils/setAuthToken";
+import { loadUser } from "./redux/actions/auth";
 import "./App.css";
 
 import { Navbar } from "./components/layout/Navbar";
@@ -10,22 +12,32 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 
-const App = () => (
-	<Provider store={store}>
-		<Router>
-			<Fragment>
-				<Navbar />
-				<Route exact path="/" component={Landing} />
-				<section className="container">
-					<Alert />
-					<Switch>
-						<Route path="/register" component={Register} />
-						<Route path="/login" component={Login} />
-					</Switch>
-				</section>
-			</Fragment>
-		</Router>
-	</Provider>
-);
+if (localStorage.token) {
+	setAuthToken(localStorage.token);
+}
+
+const App = () => {
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
+
+	return (
+		<Provider store={store}>
+			<Router>
+				<Fragment>
+					<Navbar />
+					<Route exact path="/" component={Landing} />
+					<section className="container">
+						<Alert />
+						<Switch>
+							<Route path="/register" component={Register} />
+							<Route path="/login" component={Login} />
+						</Switch>
+					</section>
+				</Fragment>
+			</Router>
+		</Provider>
+	);
+};
 
 export default App;
