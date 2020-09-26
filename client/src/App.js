d08a65a06bf3425bdbb489568824a62459a4bb73
jstonedev <jstonedev521@gmail.com>
2020-09-26 11:01:32 -0400

    modify: hide landing page once logged in

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import { loadUser } from "./redux/actions/auth";
 import "./App.css";
 
 import Navbar from "./components/layout/Navbar";
-import { Landing } from "./components/layout/Landing";
+import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
