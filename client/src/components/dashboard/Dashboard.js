f25df93bc02bb8cce412c5401b086bac883e0184
jstonedev <jstonedev521@gmail.com>
2020-09-26 11:26:06 -0400

    update: create spinner component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,14 +1,23 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../redux/actions/profile";
+import Spinner from "../layout/Spinner";
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({
+	getCurrentProfile,
+	auth,
+	profile: { profile, isLoading },
+}) => {
 	useEffect(() => {
 		getCurrentProfile();
 	}, [getCurrentProfile]);
 
-	return <div>Dashboard</div>;
+	return isLoading && profile === null ? (
+		<Spinner />
+	) : (
+		<Fragment>test</Fragment>
+	);
 };
 
 Dashboard.propTypes = {
