1c6b79d9295ceb48b35196e120b558b9735fb0ef
jstonedev <jstonedev521@gmail.com>
2020-09-28 00:00:34 -0400

    modify: setup create profile action & implement into create-profile form

--- client/src/redux/actions/profile.js
+++ client/src/redux/actions/profile.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { setAlert } from "../actions/alert";
 
 import { GET_PROFILE, PROFILE_ERROR } from "./types";
 
@@ -16,6 +17,53 @@ export const getCurrentProfile = () => async (dispatch) => {
 			type: PROFILE_ERROR,
 			payload: {
 				msg: error.response.statusText,
+			},
+		});
+	}
+};
+
+// Create or update profile
+export const createProfile = (FormData, history, edit = false) => async (
+	dispatch
+) => {
+	try {
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
+
+		const res = await axios.post("/api/profile", FormData, config);
+
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data,
+		});
+
+		dispatch(
+			setAlert(
+				edit
+					? "Profile Successfully Updated!"
+					: "Profile Successfully Created!",
+				"success"
+			)
+		);
+
+		if (!edit) {
+			history.push("/dashboard");
+		}
+	} catch (error) {
+		const errors = error.response.data.errors;
+
+		if (errors) {
+			errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+		}
+
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: {
+				msg: error.response.statusText,
+				status: error.response.status,
 			},
 		});
 	}
