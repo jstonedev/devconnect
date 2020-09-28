decbd208a551a87c01ec57bd3ed5bf8e3110c229
jstonedev <jstonedev521@gmail.com>
2020-09-28 15:10:57 -0400

    update: list education and experiences on the dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 
 import Spinner from "../layout/Spinner";
 import { DashboardActions } from "./DashboardActions";
+import Experience from "./Experience";
+import Education from "./Education";
 
 const Dashboard = ({
 	getCurrentProfile,
@@ -28,6 +30,8 @@ const Dashboard = ({
 			{profile !== null ? (
 				<Fragment>
 					<DashboardActions />
+					<Experience experience={profile.experience} />
+					<Education education={profile.education} />
 				</Fragment>
 			) : (
 				<Fragment>
