092101a0e2a2d3b919b45277adb1971d86c265cb
jstonedev <jstonedev521@gmail.com>
2020-09-28 11:55:43 -0400

    update: add education & experience forms and actions

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,8 @@ import PrivateRoute from "./components/routing/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
+import AddExperience from "./components/profile-forms/AddExperience";
+import AddEducation from "./components/profile-forms/AddEducation";
 
 if (localStorage.token) {
 	setAuthToken(localStorage.token);
@@ -46,6 +48,16 @@ const App = () => {
 								exact
 								path="/create-profile"
 								component={CreateProfile}
+							/>
+							<PrivateRoute
+								exact
+								path="/add-experience"
+								component={AddExperience}
+							/>
+							<PrivateRoute
+								exact
+								path="/add-education"
+								component={AddEducation}
 							/>
 						</Switch>
 					</section>
--- client/src/redux/actions/profile.js
+++ client/src/redux/actions/profile.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { setAlert } from "../actions/alert";
 
-import { GET_PROFILE, PROFILE_ERROR } from "./types";
+import { GET_PROFILE, UPDATE_PROFILE, PROFILE_ERROR } from "./types";
 
 // Get current users profile
 export const getCurrentProfile = () => async (dispatch) => {
@@ -49,6 +49,70 @@ export const createProfile = (FormData, history, edit = false) => async (
 		if (!edit) {
 			history.push("/dashboard");
 		}
+	} catch (error) {
+		const errors = error.response.data.errors;
+
+		if (errors) {
+			errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+		}
+
+		dispatch({
+			type: PROFILE_ERROR,
+		});
+	}
+};
+
+// Add experience
+export const addExperience = (FormData, history) => async (dispatch) => {
+	try {
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
+
+		const res = await axios.put("/api/profile/experience", FormData, config);
+
+		dispatch({
+			type: UPDATE_PROFILE,
+			payload: res.data,
+		});
+
+		dispatch(setAlert("Experience Successfully Added!", "success"));
+
+		history.push("/dashboard");
+	} catch (error) {
+		const errors = error.response.data.errors;
+
+		if (errors) {
+			errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+		}
+
+		dispatch({
+			type: PROFILE_ERROR,
+		});
+	}
+};
+
+// Add education
+export const addEducation = (FormData, history) => async (dispatch) => {
+	try {
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
+
+		const res = await axios.put("/api/profile/education", FormData, config);
+
+		dispatch({
+			type: UPDATE_PROFILE,
+			payload: res.data,
+		});
+
+		dispatch(setAlert("Education Successfully Added!", "success"));
+
+		history.push("/dashboard");
 	} catch (error) {
 		const errors = error.response.data.errors;
 
